4bce4fe94d059a89b17a7fb4a95f5ce00b4ff9f2
it18-2016 <it18-2016@AR1-ASISTENT.mshome.net>
2020-03-13 11:54:27 +0100

    project setup

---
+++ BackendRVA6/src/main/java/rva/BackendRva6Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRva6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRva6Application.class, args);
+	}
+
+}
