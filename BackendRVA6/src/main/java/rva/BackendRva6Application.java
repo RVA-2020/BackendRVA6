ade4eee3c7e42c2923eea4a7e9e78386c6060834
it18-2016 <it18-2016@AR1-ASISTENT.mshome.net>
2020-03-13 12:43:21 +0100

    vezbe 2

--- BackendRVA6/src/main/java/rva/BackendRva6Application.java
+++ BackendRVA6/src/main/java/rva/BackendRva6Application.java
@@ -1,7 +1,10 @@
 package rva;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendRva6Application {
@@ -9,5 +12,17 @@ public class BackendRva6Application {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendRva6Application.class, args);
 	}
-
+	
+	//funkcionalni interfejs
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context){
+		CommandLineRunner lambda = (args) -> {
+			System.out.println("Beans provided by Spring Framework");
+			String[] beanNames = context.getBeanDefinitionNames();
+			for(String beanName: beanNames){
+				System.out.println(beanName);
+			}
+		};
+		return lambda;
+	}
 }
---
+++ BackendRVA6/src/main/java/rva/ctrls/HelloRestController.java
@@ -0,0 +1,20 @@
+package rva.ctrls;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+	@RequestMapping("/")
+	public String hello(){
+		return "Hello World!";
+	}
+	
+	@RequestMapping("/zbir")
+	public String zbir(){
+		long x = Math.round(Math.random() * 10);
+		long y = Math.round(Math.random() * 10);
+		return x + " + " + y + " = " + (x + y);
+	}
+}
